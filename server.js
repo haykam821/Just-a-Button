290c0e8abd5f36dd268f29ec7a0033c2ac9c0856
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-10-19 18:15:16 -0400

    Use the debug library rather than Winston for logging

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const io = require("socket.io");
 
-const logger = require("winston");
-logger.level = "info";
+const debug = require("debug");
+const log = debug("just-a-button");
 
 const server = http.createServer();
 server.listen(8080);
 
-logger.info("The server has started and is now listening.");
+log("The server has started and is now listening.");
 
 const baseClickIncrease = 1;
 let scoreTotal = 0;
@@ -15,12 +15,12 @@ let scoreTotal = 0;
 const socket = io.listen(server);
 
 socket.on("connection", (client) => {
-    logger.info("A client has connected to the server.");
+    log("A client has connected to the server.");
 
     client.on("click", (message) => {
         scoreTotal += baseClickIncrease;
     });
     client.on("disconnect", (message) => {
-        logger.info("A client has disconnected from the server.");
+        log("A client has disconnected from the server.");
     });
 });
